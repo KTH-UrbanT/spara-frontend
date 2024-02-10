cc037f91a12439c2fbeb11cbccab3e59afdbffd6
pasichnyi <oleksii.pasichnyi@abe.kth.se>
2024-02-10 08:31:04 +0100

    fix of dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { SocketProvider } from "./context/socket";
 
 function App() {
   return (
-    <div className="bg-gradient-to-b from-gray-100 to-gray-300 dark:bg-gradient-to-b from-gray-700 to-gray-900 h-[calc(100dvh)] w-screen flex flex-col items-center">
+    <div className="bg-gradient-to-b from-gray-100 to-gray-300 dark:bg-gradient-to-b dark:from-gray-700 dark:to-gray-900 h-[calc(100dvh)] w-screen flex flex-col items-center">
       <SocketProvider>
         <Chat />
       </SocketProvider>
