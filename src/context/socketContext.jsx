1b708e17647cd72d6bda7a2b13042070a4f151c1
Shadaab Ghani <shadaab96ghani@gmail.com>
2026-05-20 19:48:42 +0200

    lets all pray again

--- src/context/socketContext.jsx
+++ src/context/socketContext.jsx
@@ -121,7 +121,6 @@ export const SocketProvider = ({ children }) => {
         removeMessageListener(handleMessageReceive);
         removeAnswerListener(handleAnswerReceive);
         removeSessionUpdatedListener(handleSessionUpdate);
-        socket.disconnect();
       };
     }
 
